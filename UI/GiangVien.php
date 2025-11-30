41181e2cdcf2e48b10dd20204085280834b58790
HaruAlex <yn84258@gmail.com>
2025-11-30 22:24:34 +0700

    fix trang chủ của giảng viên, mấy quỷ kia chưa làm

---
+++ UI/GiangVien.php
@@ -0,0 +1,40 @@
+
+<!-- Sidebar -->
+<div class="sidebar">
+    <h2 style="margin-bottom: 25px;">Giảng viên</h2>
+
+    <a href="TrangChuGV.php">Trang chủ</a>
+
+    <div class="dropdown">
+        <span class="dropbtn">Quản lý lớp học phần</span>
+        <div class="dropdown-content">
+            <a href="#">Danh sách lớp sinh viên</a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <span class="dropbtn">Quản lý danh sách môn học</span>
+        <div class="dropdown-content">
+            <a href="#">Danh sách môn học</a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <span class="dropbtn">Quản lý điểm danh</span>
+        <div class="dropdown-content">
+            <a href="#">Tạo biểu điểm danh</a>
+            <a href="#">Cập nhập biểu điểm danh</a>
+        </div>
+    </div>
+
+    <div class="dropdown">
+        <span class="dropbtn">Quản lý danh sách điểm danh</span>
+        <div class="dropdown-content">
+            <a href="#">Danh sách phiên</a>
+        </div>
+    </div>
+
+    <a href="#">Thống kê chuyên cần</a>
+
+    <a href="#">Đăng xuất</a>
+</div>
