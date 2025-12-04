b64bc6175869958b2b56bd5aea944610132eea07
HaruAlex <yn84258@gmail.com>
2025-12-04 12:44:04 +0700

    ..

---
+++ admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="admin.css">
+    <?php include('menuad.php'); ?>
+</head>
+<body>
+
+<div class="wrapper">
+
+    <main class="content">
+        <h1 class="page-title">Phần mềm quản lý ĐIỂM DANH online</h1>
+        <div class="table-box">
+            <p>➺Điểm danh trực tuyến</p>
+            <p>➺Thống kê điểm chuyên cần</p>
+        </div>
+    </main>
+
+</div>
+
+</body>
+</html>
